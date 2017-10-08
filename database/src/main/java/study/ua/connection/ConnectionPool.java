23b45b7ac3a1fbb66a13a8bd293281670c18f7d5
sergeyonischuk <sergey.onischuk@ukr.net>
2017-10-09 00:06:47 +0300

    pull connection

--- database/src/main/java/study/ua/connection/ConnectionPool.java
+++ database/src/main/java/study/ua/connection/ConnectionPool.java
@@ -1,4 +1,38 @@
 package study.ua.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.log4j.Log4j;
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+@Log4j
 public class ConnectionPool {
+    private static ConnectionPool connectionPool;
+    private static HikariDataSource dataSource;
+    private static final Integer CONNECTION_POOL_SIZE = 10;
+    private ConnectionPool(){}
+
+    public static ConnectionPool getConnectionPoolInstance() {
+        if(connectionPool == null) {
+            connectionPool = new ConnectionPool();
+            dataSource = new HikariDataSource();
+            dataSource.setUsername("root");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/busbase");
+            dataSource.setPassword("root");
+            dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+            dataSource.setMaximumPoolSize(CONNECTION_POOL_SIZE);
+        }
+        return connectionPool;
+    }
+
+    public Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            log.error(e);
+        }
+        throw new IllegalArgumentException("Cannot get connection");
+    }
+
 }
--- database/src/main/java/study/ua/connection/DaoFactory.java
+++ database/src/main/java/study/ua/connection/DaoFactory.java
@@ -1,54 +1,34 @@
-package DAO;
+package study.ua.connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
-
-
-public class FactoryDAO {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/busbase";
-    private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "root";
-
-    private Connection connection;
-
-    public FactoryDAO() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            System.out.println(connection.getCatalog());
-            System.out.println(connection.getSchema());
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
+import lombok.extern.log4j.Log4j;
+import study.ua.DAO.*;
+@Log4j
+public class DaoFactory {
     public ApplicationDAO getApplicationDAO() {
-        return new ApplicationDAO(connection);
+        return new ApplicationDAO();
     }
     public BusDAO getBusDAO() {
-        return new BusDAO(connection);
+        return new BusDAO();
     }
     public CityDAO getCityDAO() {
-        return new CityDAO(connection);
+        return new CityDAO();
     }
     public ConfirmedAppDAO getConfirmedAppDAO() {
-        return new ConfirmedAppDAO(connection);
+        return new ConfirmedAppDAO();
     }
     public DriverDAO getDriverDAO() {
-        return new DriverDAO(connection);
+        return new DriverDAO();
     }
     public RouteDAO getRouteDAO() {
-        return new RouteDAO(connection);
+        return new RouteDAO();
     }
     public UserDAO getUserDAO() {
-        return new UserDAO(connection);
+        return new UserDAO();
     }
     public BusDriverDAO getBusDriverDAO() {
-        return new BusDriverDAO(connection);
+        return new BusDriverDAO();
     }
     public DriverUserDAO getDriverUserDAO() {
-        return new DriverUserDAO(connection);
+        return new DriverUserDAO();
     }
 }
