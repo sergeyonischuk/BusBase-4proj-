17b7a0f582b1f5d7a5fc3e22d84e96242dd277ad
sergeyonischuk <sergey.onischuk@ukr.net>
2017-10-09 00:09:02 +0300

    filters added

--- web/src/main/java/study/ua/filter/EncodingFilter.java
+++ web/src/main/java/study/ua/filter/EncodingFilter.java
@@ -1,4 +1,25 @@
 package study.ua.filter;
+import lombok.extern.log4j.Log4j;
 
-public class EncodingFilter {
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+@Log4j
+@WebFilter("/busbase/*")
+public class EncodingFilter implements Filter {
+    private static final String ENCODING = "utf-8";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding(ENCODING);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
 }
