b185d4a6a2016e809aad90ff585e8ad73c199866
sergeyonischuk <sergey.onischuk@ukr.net>
2017-10-09 00:09:55 +0300

    Login command changed

--- web/src/main/java/study/ua/commands/LoginCommand.java
+++ web/src/main/java/study/ua/commands/LoginCommand.java
@@ -1,36 +1,40 @@
 package study.ua.commands;
 
-import DAO.FactoryDAO;
-import DAO.UserDAO;
-import enums.UserType;
-import services.SecurityService;
+import lombok.extern.log4j.Log4j;
+import study.ua.entityes.User;
+import study.ua.enums.UserType;
+import study.ua.services.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
-public class Login implements Command{
+@Log4j
+public class LoginCommand implements Command {
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         String login = req.getParameter("username");
         String password = req.getParameter("password");
-        UserDAO userDAO = new FactoryDAO().getUserDAO();
-        SecurityService securityService = new SecurityService();
-        boolean validateUser = securityService.validateUser(login, password);
+        UserService userService = new UserService();
+        boolean validateUser = userService.validateUser(login, password);
 
         if (validateUser) {
-            UserType type = userDAO.getUserType(login, password);
-                req.getSession().setAttribute("username", login);
-                req.setAttribute("usertype", type);
-                req.getSession().setAttribute("type", type.name());
-                if (type.equals(UserType.DISPATCHER)) {
-                    return "dispatcherPage.jsp";
-                } else if (type.equals(UserType.DRIVER)) {
-                    return "driverPage.jsp";
-                }
+            User user = userService.getUserByLogin(login);
+            UserType type = userService.getUserType(login);
+            HttpSession session = req.getSession();
+            session.setAttribute("user", user);
+            session.setAttribute("username", login);
+
+            if (type.equals(UserType.DISPATCHER)) {
+                log.info("Dispatcher " + login + " logged in");
+                return "dispatcherPage.jsp";
+            } else if (type.equals(UserType.DRIVER)) {
+                log.info("Driver " + login + " logged in");
+                return "driverPage.jsp";
             }
+        }
         return "loginError.jsp";
     }
 }
